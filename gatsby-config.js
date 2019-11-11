1770e64f92b70461be0e79e3c04e520964076320
Samuel Taylor Admin <taylors.admin@MoneyAdvice.net>
2019-11-11 11:40:54 +0000

    First Commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `My Superblog!`,
+    description: `Gatsby is what this blog uses`,
+    author: `@gatsbyjs & @Samueltaylor001`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -11,6 +11,19 @@ module.exports = {
       options: {
         name: `images`,
         path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: `gatsby-source-strapi`,
+      options: {
+        apiURL: `http://localhost:1337`,
+        contentTypes: [
+          `article`,
+          `user`
+        ],
+        queryLimit: 1000,
+
+
       },
     },
     `gatsby-transformer-sharp`,
